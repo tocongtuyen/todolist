cbf36c712f5b917f8e27e4778b6ea2ec1e5da20d
tocongtuyen <tocongtuyen@gmail.com>
2020-05-11 22:01:03 +0700

    add screen todolist and modal detail

---
+++ src/model/tempData.js
@@ -0,0 +1,46 @@
+export default (tempData = [
+  {
+    name: 'Plan a Trip',
+    color: '#24A6D9',
+    todos: [
+      {
+        title: 'Book Flight',
+        completed: false,
+      },
+      {
+        title: 'Passpart Check',
+        completed: false,
+      },
+      {
+        title: 'Reserve Hptel Room',
+        completed: true,
+      },
+      {
+        title: 'Pack Luggage',
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: 'Errands',
+    color: '#882209',
+    todos: [
+      {
+        title: 'Buy Milk',
+        completed: true,
+      },
+      {
+        title: 'Passpart Check',
+        completed: true,
+      },
+      {
+        title: 'Reserve Hptel Room',
+        completed: true,
+      },
+      {
+        title: 'Pack Luggage',
+        completed: false,
+      },
+    ],
+  },
+]);
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,68 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Alert,
+  KeyboardAvoidingView,
+  TextInput,
+} from 'react-native';
 import {useTheme} from '@react-navigation/native';
+import colors from '../config/Colors';
+import tempData from '../model/tempData';
+import TodoList from '../components/TodoList';
+import AddListModal from '../components/AddListModal';
 
 const HomeScreen = ({navigation}) => {
   const {colors} = useTheme();
-
   const theme = useTheme();
+  const [modalVisible, setModalVisible] = useState(false);
 
+  const renderList = list => {
+    return <TodoList list={list} />;
+  };
   return (
     <View style={styles.container}>
       <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
-      <Text style={{color: colors.text}}>Home Screen</Text>
-      <Button
-        title="Go to details screen"
-        onPress={() => navigation.navigate('Details')}
-      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}>
+        <AddListModal closeModal={() => setModalVisible(false)} />
+      </Modal>
+      <View style={{flexDirection: 'row'}}>
+        <View style={styles.divider} />
+        <Text style={styles.title}>
+          Todo <Text style={{fontWeight: '300', color: '#24A6D9'}}>Lists</Text>
+        </Text>
+        <View style={styles.divider} />
+      </View>
+
+      <View style={{marginVertical: 48}}>
+        <TouchableOpacity
+          style={styles.addList}
+          onPress={() => setModalVisible(true)}>
+          <Text style={{fontSize: 16, color: '#24A6D9'}}>+</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.add}>Add List</Text>
+      </View>
+      <View style={{height: 275, paddingLeft: 32}}>
+        <FlatList
+          data={tempData}
+          keyExtractor={item => item.name}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({item}) => renderList(item)}
+        />
+      </View>
     </View>
   );
 };
@@ -24,7 +72,34 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: '800',
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: '600',
+    fontSize: 14,
+    marginTop: 8,
   },
 });
